e9deac931e4b1c35793668cf723730fc2fc64075
nsflemming <70294403+nsflemming@users.noreply.github.com>
2023-11-01 09:50:47 -0400

    Update L2_data_merge.R

--- L2_data_merge.R
+++ L2_data_merge.R
@@ -7,10 +7,21 @@ library(data.table)
 
 #read in data
 setwd("C:/Users/natha/Desktop/Polling Places/data")
-L2<-read.csv('PA_combined.csv', nrows = 1)
-test<-fread('PA_combined.csv', 
-            select = c('LALVOTERID','Voters_FirstName_2016','Voters_MiddleName_2016',
-                       'Voters_LastName_2016','Voters_NameSuffix_2016',
+row1<-read.csv('PA_combined.csv', nrows = 1)
+L2<-fread('PA_combined.csv', 
+            select = c('LALVOTERID',
+                       'Residence_Addresses_AddressLine_2020','Residence_Addresses_ExtraAddressLine_2020',
+                       'Residence_Addresses_City_2020','Residence_Addresses_State_2020',
+                       'Residence_Addresses_Zip_2020',
+                       'Residence_Addresses_Latitude_2020','Residence_Addresses_Longitude_2020',
+                       'Voters_Gender_2020','Voters_Age_2020','Parties_Description_2020',
+                       'Religions_Description_2020','Voters_OfficialRegDate_2020',
+                       'MaritalStatus_Description_2020','CommercialData_PresenceOfChildrenCode_2020',
+                       'CommercialData_EstimatedHHIncomeAmount_2020',
+                       'CommercialData_Education_2020','County_2020','Voters_FIPS_2020',
+                       'Voters_Active_2020','CountyEthnic_LALEthnicCode_2020',
+                       'EthnicGroups_EthnicGroup1Desc_2020','Ethnic_Description_2020',
+                       'CountyEthnic_Description_2020'
                                           ))
 poll<-read.csv('polllocation_structure_and_keyword.csv')
 
