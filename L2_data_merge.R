1c9db09ec7dd1dd77f0b360aac4cc6a6415db948
nsflemming <70294403+nsflemming@users.noreply.github.com>
2023-11-01 14:40:37 -0400

    Update L2_data_merge.R

--- L2_data_merge.R
+++ L2_data_merge.R
@@ -7,7 +7,7 @@ library(data.table)
 
 #read in data
 setwd("C:/Users/natha/Desktop/Polling Places/data")
-row1<-read.csv('PA_combined.csv', nrows = 1)
+#row1<-read.csv('PA_combined.csv', nrows = 1)
 L2<-fread('PA_combined.csv', 
             select = c('LALVOTERID',
                        'Residence_Addresses_AddressLine_2020','Residence_Addresses_ExtraAddressLine_2020',
@@ -26,7 +26,20 @@ L2<-fread('PA_combined.csv',
 poll<-read.csv('polllocation_structure_and_keyword.csv')
 
 
+### remove obs if missing address
+L2 <- L2 %>%
+  drop_na(Residence_Addresses_AddressLine_2020)
 
+###combine voter coordinates into one so arcGIS can split it
+L2$voterLongLat<-paste0(L2$Residence_Addresses_Latitude_2020,',',L2$Residence_Addresses_Longitude_2020)
+L2$voterLongLat<-str_remove(L2$voterLongLat, " , ")
+
+### get just id and coordinates
+L2_mini<-subset(L2, select = c(LALVOTERID, voterLongLat))
+
+### save to csv
+setwd("C:/Users/natha/Desktop/Polling Places/data")
+write.csv(L2_mini, 'L2_2020_coords')
 
 
 
