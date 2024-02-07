d30f9574e281fc5445a4bed1442c1e87a032bc76
nsflemming <70294403+nsflemming@users.noreply.github.com>
2024-02-07 15:25:30 -0500

    Finalize L2 merge code.

--- L2_Raw_Processing.R
+++ L2_Raw_Processing.R
@@ -3,12 +3,33 @@
 
 library(tidyverse)
 library(data.table) #read in L2 data selectively
+library(stringr) #string manipulation
 
+# Read in L2 data selectively
 get_L2_data <- function(L2_dir, filename, vars){
   setwd(L2_dir)
   L2<-fread(filename, sep='\t', header=T, select = vars)
   return(L2)
 }
+
+# Read in precinct mapping crosswalk
+get_match_data <- function(match_dir, filename, vars){
+  setwd(match_dir)
+  match<-read.csv(filename)
+  match<-subset(match, select = vars)
+  #remove the apostrophe from the precinct ids
+  match$precinct_id = str_extract(match$precinct_id, "(?<=\\')(.*)")
+  return(match)
+}
+
+# Read in poll location data
+get_poll_data <- function(poll_dir, filename, vars){
+  setwd(poll_dir)
+  poll<-read.csv(filename)
+  ## remove unneeded columns
+  poll <- subset(poll, select = vars)
+  return(poll)
+  }
 
 
 
@@ -42,20 +63,33 @@ L2votehist <-get_L2_data(L2_dir, 'VM2--PA--2019-08-22-VOTEHISTORY.tab', vote_var
 L2demog<-get_L2_data(L2_dir, 'VM2--PA--2019-08-22-DEMOGRAPHIC.tab', demog_vars)
 # Combine L2 data
 L2demog<-left_join(L2demog, L2votehist, by = 'LALVOTERID')
-#### save to csv
+### drop voterhistory
+rm(L2votehist)
+
+################# merge L2 data with matched precincts
+### Read manually matched L2 and poll location precinct names back in
+match<-get_match_data(data_dir, 'joined_sheet_matched.csv', 
+                    c('county_pre', 'county_name', 'precinct_name', 'precinct_id'))
+### Merge precinct and county names in L2 data frame
+L2demog$county_pre = paste0(L2demog$County,' - ',L2demog$Precinct)
+## merge L2 with joined_sheet on L2 precinct names
+L2demog<-left_join(L2demog, match, by='county_pre')
+## Drop crosswalk
+rm(match)
+
+################# merge in polling place categories
+# Read in location/category data
+poll <- get_poll_data(data_dir, 'polllocation_structure_and_keyword.csv', 
+              c('county_name', 'precinct_name', 'location_category'))
+# Join in poll location data
+L2demog <- left_join(L2demog, poll, by=c('county_name', 'precinct_name'))
+## drop location 
+rm(poll)
+
+################# write to csv
 setwd(data_dir)
 write.csv(L2demog, 'L2PA_full.csv')
 
 
 
 
-
-
-
-
-
-
-
-
-
-##############################
