c11a7447745c8cbb919d3cd00a1d12fd7f8fb992
nsflemming <70294403+nsflemming@users.noreply.github.com>
2024-05-10 10:20:54 +0100

    Update multiple_category_crosstab.R
    Begin bringing into line with current categorization used in analysis.

--- multiple_category_crosstab.R
+++ multiple_category_crosstab.R
@@ -1,7 +1,7 @@
 #Nathaniel Flemming
 # 11/24/23
 
-# check whether keyword codings agree with authoritative structure data
+# 
 library(tidyverse)
 library(purrr) # for summing rows
 
@@ -10,32 +10,20 @@ setwd("C:/Users/natha/Desktop/Polling Places/data/Structures")
 gov_struct_matrix<-read.csv('structure_matrix.csv') #pure structure matrix
 keyword_matrix <- read.csv('keyword_matrix.csv') #pure keyword matrix
 setwd("C:/Users/natha/Desktop/Polling Places/data")
-poll<-read.csv('polllocation_and_structure.csv') #poll + structure condensed
+poll<-read.csv('poll_struct_key_govsource18.csv') #structure and keyword coded locations
 
-################
+#combine structure and keyword matrices
+test<-left_join(gov_struct_matrix, keyword_matrix, by='X')
+
 #location category count instead of location category to see what's multiple coded
-gov_struct_matrix<-gov_struct_matrix%>%
-  group_by(address, precinct_id)%>%
-  mutate(location_count = sum(across(c(religious,school,public,
-                                       justice,postoffice,
-                                       rangerstation,allegh_pub,library)))) %>%
-  ungroup()%>%
-  select(!c(polling_place_id, source_notes))
-#change locations with two categories to 'multiple' to check missingness
-gov_struct_matrix$location_category[gov_struct_matrix$location_count>1]<-'multiple'
-# subset out multiple category locations
-gov_struct_mult = gov_struct_matrix[gov_struct_matrix$location_count>1,]
-## remove missing
-gov_struct_mult<-gov_struct_mult[complete.cases(gov_struct_mult),]
-## distribution of multiple location count variable
-summary(gov_struct_mult$location_count)
-ggplot(data=gov_struct_mult, aes(x=location_count))+
-  geom_histogram(bins=4)
-sum(gov_struct_mult$location_count==5)/5 # 1 5-category locations
-sum(gov_struct_mult$location_count==4)/4 # 11 4-category locations
-sum(gov_struct_mult$location_count==3)/3 # 56 3-category locations
-sum(gov_struct_mult$location_count==2)/2 # 344 2-category locations
-
+for(i in seq_along(test)){
+  temp<-test[[i]]%>%
+    group_by(address, PrecinctCode)%>%
+    mutate(location_count = sum(across(c(religious,school,public,justice,other,
+                                         #allegh_pub,
+                                         library))))%>%
+    ungroup()
+}
 ##########  two category locations crosstab
 two_category<-gov_struct_mult[gov_struct_mult$location_count==2,]
 ## create two category columns for each row
@@ -46,9 +34,7 @@ two_category<-two_category%>%
                                 school==1 ~ 'school',
                                 public==1 ~ 'public',
                                 allegh_pub==1 ~ 'allegh_pub',
-                                postoffice==1 ~ 'postoffice',
                                 justice==1 ~ 'justice',
-                                rangerstation==1 ~ 'rangerstation',
                                 library==1 ~ 'library',
                                 .default = location_category))
 
@@ -75,9 +61,7 @@ three_category<-three_category%>%
                                 school==1 ~ 'school',
                                 public==1 ~ 'public',
                                 allegh_pub==1 ~ 'allegh_pub',
-                                postoffice==1 ~ 'postoffice',
                                 justice==1 ~ 'justice',
-                                rangerstation==1 ~ 'rangerstation',
                                 library==1 ~ 'library',
                                 .default = location_category))
 
@@ -98,3 +82,48 @@ three_cat_crosstab<-ftable(mytable)
 three_cat_crosstab
 
 
+
+
+
+### Number of multiple coded locations that aren't religious school or public justice
+sum((poll$location_count==3)|(poll$keyword_count==3), na.rm = T) # 54 3-category locations
+sum((poll$location_count==2)|(poll$keyword_count==2), na.rm = T) # 1041 2-category locations
+#### Combine structure and keyword count, taking higher
+poll<-poll%>%
+  mutate(max_count=pmax(location_count, keyword_count))
+two_category<-poll[poll$max_count==2,]
+
+
+output<-data.frame()
+for (x in seq(from=1, to=nrow(two_category)-1, by=2)){
+  y=x+1 #don't know why this is needed
+  temp <- two_category[x:y,]
+  temp$category_2 <- temp$category_1[2]
+  output<-rbind(output,(temp[1,]))
+}
+
+################
+#location category count instead of location category to see what's multiple coded
+gov_struct_matrix<-gov_struct_matrix%>%
+  group_by(address, precinct_id)%>%
+  mutate(location_count = sum(across(c(religious,school,religious,public,
+                                       justice,
+                                       allegh_pub,library)))) %>%
+  ungroup()%>%
+  select(!c(polling_place_id, source_notes))
+#add flag for multiple categorizations
+gov_struct_matrix$multiple_flag[gov_struct_matrix$location_count>1]<-'multiple'
+# subset out multiple category locations
+gov_struct_mult = gov_struct_matrix[gov_struct_matrix$location_count>1,
+                                    c('name','location_count','location_category','multiple_flag',
+                                      'religious','school','public','allegh_pub','justice','library')]
+## remove missing
+gov_struct_mult<-gov_struct_mult[complete.cases(gov_struct_mult),]
+## distribution of multiple location count variable
+summary(gov_struct_mult$location_count)
+ggplot(data=gov_struct_mult, aes(x=location_count))+
+  geom_histogram(bins=2)
+sum(gov_struct_mult$location_count==5)/5 # 0 5-category locations
+sum(gov_struct_mult$location_count==4)/4 # 0 4-category locations
+sum(gov_struct_mult$location_count==3)/3 # 26 3-category locations
+sum(gov_struct_mult$location_count==2)/2 # 299 2-category locations
