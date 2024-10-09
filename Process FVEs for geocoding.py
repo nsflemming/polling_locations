e34ab123c364caea5ff651f772b97f4e168909d9
nsflemming <70294403+nsflemming@users.noreply.github.com>
2024-10-09 08:07:53 -0400

    Update Process FVEs for geocoding.py
    Update file paths and counties

--- Process FVEs for geocoding.py
+++ Process FVEs for geocoding.py
@@ -9,20 +9,20 @@ import os
 
 ############# Convert FVE files to Census geocoding format
 ## county names
-'''counties = ['ADAMS', 'ALLEGHENY', 'ARMSTRONG', 'BEAVER', 'BEDFORD', 'BERKS', 'BLAIR', 'BRADFORD', 'BUCKS', 'BUTLER',
+counties = ['ADAMS', 'ALLEGHENY', 'ARMSTRONG', 'BEAVER', 'BEDFORD', 'BERKS', 'BLAIR', 'BRADFORD', 'BUCKS', 'BUTLER',
             'CAMBRIA', 'CAMERON', 'CARBON', 'CENTRE', 'CHESTER', 'CLARION', 'CLEARFIELD', 'CLINTON', 'COLUMBIA',
             'CRAWFORD', 'CUMBERLAND', 'DAUPHIN', 'DELAWARE', 'ELK', 'ERIE', 'FAYETTE', 'FOREST', 'FRANKLIN', 'FULTON',
             'GREENE', 'HUNTINGDON', 'INDIANA', 'JEFFERSON', 'JUNIATA', 'LACKAWANNA', 'LANCASTER', 'LAWRENCE', 'LEBANON',
             'LEHIGH', 'LUZERNE', 'LYCOMING', 'MCKEAN', 'MERCER', 'MIFFLIN', 'MONROE', 'MONTGOMERY', 'MONTOUR',
             'NORTHAMPTON', 'NORTHUMBERLAND', 'PERRY', 'PHILADELPHIA', 'PIKE', 'POTTER', 'SCHUYLKILL', 'SNYDER',
             'SOMERSET', 'SULLIVAN', 'SUSQUEHANNA', 'TIOGA', 'UNION', 'VENAGO', 'WARREN', 'WASHINGTON', 'WAYNE',
-            'WESTMORELAND', 'WYOMING', 'YORK']'''
-counties = ['PHILADELPHIA']
+            'WESTMORELAND', 'WYOMING', 'YORK']
+#counties = ['PHILADELPHIA']
 # create list of paths to FVE files
-FVEpath = 'C:/Users/natha/OneDrive/Desktop/Polling Places/data/Raw FVEs/'
+FVEpath = 'C:/Users/natha/Desktop/Polling Places DiD/data/Raw FVEs 2020/'
 FVEfiles = [i for i in os.listdir(FVEpath) if os.path.isfile(os.path.join(FVEpath,i)) and
          any(sub in i for sub in counties) and # include file if it has a county in its name
-         '20191028.txt' in i]  # include file if it has 20191028.txt in its name
+         'FVE' in i]  # include file if it has 20191028.txt in its name
 FVEfilepaths = [FVEpath + file for file in FVEfiles]  # add path to file name
 
 ## Read in raw FVEs and reformat for geocoding
@@ -50,7 +50,7 @@ for FVE in FVEfilepaths:
     addressdfs = [address[i:i+n] for i in range(0, len(address), n)]
     # save each data frame as a procedurally named file
     ## create path
-    savepath = 'C:/Users/natha/OneDrive/Desktop/Polling Places/data/FVEs for geocode/'
+    savepath = 'C:/Users/natha/Desktop/Polling Places DiD/data/FVEs for geocode/'
     for slice in addressdfs:
         m = str(n) #convert to string
         slice_path = savepath+counties[i]+'_FVE_'+m+'.csv'  # create incrementing path
