58658c83b505d56fc91d64f62f73c1fac62ad86d
nsflemming <70294403+nsflemming@users.noreply.github.com>
2024-10-24 11:17:30 -0400

    Update establishing_parallel_trends.R
    Modify script to work for 3 years.

--- establishing_parallel_trends.R
+++ establishing_parallel_trends.R
@@ -1,6 +1,6 @@
 #Nathaniel Flemming
 # 24/9/24
-# 17/10/24
+# 23/10/24
 
 # Checking assumption for DiD, using data created by DiD_preprocessing script
 
@@ -22,18 +22,18 @@ results_dir <-"C:/Users/natha/Desktop/Polling Places DiD/model_results"
 plot_dir <- "C:/Users/natha/Desktop/Polling Places DiD/plots"
 # read in data
 setwd(data_dir)
-model_data<-read.csv('DiD_prepped_loc_vote_17_18.csv')
+model_data<-read.csv('DiD_prepped_loc_vote_16to18.csv')
 
 # Modify variables to fit DiD package requirements
 ## ID
 model_data$VOTERID<-str_sub(model_data$LALVOTERID, 6, nchar(model_data$LALVOTERID))
 model_data$VOTERID<-as.numeric(model_data$VOTERID)
-# set years to 0 and 1
+# set years to 2017 = 0
 model_data$year<-model_data$year-2017
 ## treatment indicators
 model_data<-model_data%>%
   group_by(LALVOTERID)%>%
-  mutate(ever_changed_poll_loc=sum(changed_poll_loc),
+  mutate(ever_changed_precinct=sum(changed_prec),
          ever_no_move_new_precinct=sum(no_move_new_precinct),
          ever_moved_new_precinct=sum(moved_new_precinct))%>%
   ungroup()
@@ -41,19 +41,24 @@ model_data<-model_data%>%
 #Trim data
 ##### proof of concept, currently uses wrong election years
 plot_data<-model_data%>%
-  select(c(year, General_2018_11_06,General_2019_11_05,ever_no_move_new_precinct))
-# split data by group
+  select(c(year, General_2016_11_08, General_2017_11_07,General_2018_11_06,
+           General_2019_11_05,ever_changed_precinct, ever_no_move_new_precinct,
+           ever_moved_new_precinct))
+
+# split data by group (treatment vs. control)
 pd_t<-plot_data[plot_data$ever_no_move_new_precinct==1,]
 pd_c<-plot_data[plot_data$ever_no_move_new_precinct==0,]
-
-pd_t$turnout[pd_t$year==0]<-sum(pd_t$General_2018_11_06, na.rm = T)/nrow(pd_t)
-pd_t$turnout[pd_t$year==1]<-sum(pd_t$General_2019_11_05, na.rm = T)/nrow(pd_t)
+## calculate turnout for each group 
+pd_t$turnout[pd_t$year==-1]<-sum(pd_t$General_2016_11_08, na.rm = T)/nrow(pd_t)
+pd_t$turnout[pd_t$year==0]<-sum(pd_t$General_2017_11_07, na.rm = T)/nrow(pd_t)
+pd_t$turnout[pd_t$year==1]<-sum(pd_t$General_2018_11_06, na.rm = T)/nrow(pd_t)
 pd_t<-pd_t%>%
   select(c(year,turnout,ever_no_move_new_precinct))%>%
   distinct()
 
-pd_c$turnout[pd_c$year==0]<-sum(pd_c$General_2018_11_06, na.rm = T)/nrow(pd_c)
-pd_c$turnout[pd_c$year==1]<-sum(pd_c$General_2019_11_05, na.rm = T)/nrow(pd_c)
+pd_c$turnout[pd_c$year==-1]<-sum(pd_c$General_2016_11_08, na.rm = T)/nrow(pd_c)
+pd_c$turnout[pd_c$year==0]<-sum(pd_c$General_2017_11_07, na.rm = T)/nrow(pd_c)
+pd_c$turnout[pd_c$year==1]<-sum(pd_c$General_2018_11_06, na.rm = T)/nrow(pd_c)
 pd_c<-pd_c%>%
   select(c(year,turnout,ever_no_move_new_precinct))%>%
   distinct()
@@ -62,14 +67,22 @@ pd_c<-pd_c[complete.cases(pd_c),]
 #recombine groups
 pd<-rbind(pd_t,pd_c)
 
+# factor treatment variables for plotting
+pd$ever_no_move_new_precinct<-factor(pd$ever_no_move_new_precinct)
+
 ggplot(pd,aes(x=year,y=turnout,colour=ever_no_move_new_precinct)) +
   geom_point()+
-  geom_line(aes(group = ever_no_move_new_precinct))+
+  geom_line(aes(group = ever_no_move_new_precinct), linetype = 2)+
   ylab("% Turnout") +
   xlab("Year") +
-  ggtitle("Parallel Trends?") +
-  theme_minimal()+
-  theme(legend.position="none")
+  ggtitle("Parallel Trends Plot") +
+  scale_x_continuous(breaks=seq(-1,1,1))+
+  #convert turnout to percentage
+  scale_y_continuous(labels = scales::percent)+
+  scale_color_discrete(name="Changed Precinct Without Moving",
+                        breaks=c(0, 1),
+                        labels=c("No", "Yes"))+
+  theme_minimal()
 
 
 
