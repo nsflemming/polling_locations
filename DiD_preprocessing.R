cc84dfd1500d1f137e2664fb2165759afbc06faf
nsflemming <70294403+nsflemming@users.noreply.github.com>
2025-10-23 22:22:52 -0400

    Update DiD_preprocessing.R
    Change variable names to work with the 2017 data files.

--- DiD_preprocessing.R
+++ DiD_preprocessing.R
@@ -71,43 +71,48 @@ rm(hist_poll_2020)
 
 # read in poll location data
 poll_2019<-read_add_year(data_dir, 'L2PA_poll_loc_VM2_19.csv',2019)
-poll_2018<-read_add_year(data_dir, 'L2PA_poll_loc_VM2_18.csv',2018)
+poll_2017<-read_add_year(data_dir, 'L2PA_poll_loc_VM2_17.csv',2017)
 # Read in Religious census data
 #setwd(data_dir)
 #relig_census<-read_excel('2020_USRC_Group_Detail.xlsx', sheet = '2020 Group by County')
 # Read in distance to polling station data (distance in degrees)
-dist_data18<-read.csv(paste0(data_dir,'\\voterloc_pollloc_dist_2018.csv'))
+dist_data17<-read.csv(paste0(data_dir,'\\voterloc_pollloc_dist_2017.csv'))
 dist_data19<-read.csv(paste0(data_dir,'\\voterloc_pollloc_dist_2019.csv'))
 
 ### read in voter demographic and address data
 ## remove county and precinct columns since that's in the voter history/polling location files
-demog_addr_2018<-read.csv(paste0(data_dir,'\\L2PA_2018_address_in_18.csv'))%>%
+demog_addr_2017<-read.csv(paste0(data_dir,'\\L2PA_2017_address_in_17.csv'))%>%
   select(-c(X, County, Precinct, Voters_FIPS, pub_loc, pub_just,
             other, relig_loc, school, multiple, justice_loc, library, relig_school))
 demog_addr_2019<-read.csv(paste0(data_dir,'\\L2PA_2019_address_in_19.csv'))%>%
   select(-c(X, County, Precinct, Voters_FIPS, pub_loc, pub_just,
             other, relig_loc, school, multiple, justice_loc, library, relig_school))
 #### Merge poll location data with voter demographic data on L2 voterid
-demog_addr_2018<-left_join(demog_addr_2018, poll_2018, by='LALVOTERID')
+demog_addr_2017<-left_join(demog_addr_2017, poll_2017, by='LALVOTERID')
 demog_addr_2019<-left_join(demog_addr_2019, poll_2019, by='LALVOTERID')
 # remove data frames to free up memory
-rm(poll_2018, poll_2019)
+rm(poll_2017, poll_2019)
 
 #### Merge distance to poll station data with voter demographic data on L2 voterid
-demog_addr_2018<-left_join(demog_addr_2018, dist_data18, by='LALVOTERID')
+demog_addr_2017<-left_join(demog_addr_2017, dist_data17, by='LALVOTERID')
 demog_addr_2019<-left_join(demog_addr_2019, dist_data19, by='LALVOTERID')
 # remove data frames to free up memory
-rm(dist_data18, dist_data19)
+rm(dist_data17, dist_data19)
 
 #### rbind years together
 ## make column names and number match (check that income variables are equivalent)
-demog_addr_2018<-demog_addr_2018%>%
-  rename(pred.whi=pred.whi_2018, pred.bla=pred.bla_2018, pred.his=pred.his_2018, 
-         pred.asi=pred.asi_2018, pred.oth=pred.oth_2018)
+demog_addr_2017<-demog_addr_2017%>%
+  rename(pred.whi=pred.whi_2017, pred.bla=pred.bla_2017, pred.his=pred.his_2017, 
+         pred.asi=pred.asi_2017, pred.oth=pred.oth_2017,
+         # 2017 variables to 2019 names
+         CommercialData_EstimatedHHIncomeAmount=CommercialData_EstimatedIncomeAmount
+         )%>%
+  # Move columns to 2019 order
+  relocate(CommercialData_OccupationGroup, .before = CommercialData_OccupationIndustry)
 demog_addr_2019<-demog_addr_2019%>%
   rename(pred.whi=pred.whi_2019, pred.bla=pred.bla_2019, pred.his=pred.his_2019, 
          pred.asi=pred.asi_2019, pred.oth=pred.oth_2019)
-poll_demog_addr_all<-rbind(demog_addr_2018, demog_addr_2019)
+poll_demog_addr_all<-rbind(demog_addr_2017, demog_addr_2019)
 ## create race variable for cross-sectional logistic regression
 #poll_demog_addr_all<- replace_na_with_neg_inf(poll_demog_addr_all,c("pred.whi","pred.bla","pred.his",
 #                                      "pred.asi","pred.oth"))
@@ -116,23 +121,23 @@ poll_demog_addr_all<-rbind(demog_addr_2018, demog_addr_2019)
 poll_demog_addr_all<-most_prob_race(poll_demog_addr_all,c("pred.whi","pred.bla","pred.his",
                                            "pred.asi","pred.oth"))
 # remove data frames to free up memory
-rm(demog_addr_2018, demog_addr_2019)
+rm(demog_addr_2017, demog_addr_2019)
 
 #### Merge in voting history on L2 voterid
 vote_poll_demog_addr_all<-left_join(poll_demog_addr_all, hist_2020, by='LALVOTERID')
 # remove data frames to free up memory
 rm(hist_2020, poll_demog_addr_all)
 
-### merge in 2018 election records from 2019 file to replace missings in 2020 file
-hist_2019<-select(hist_2019, c('LALVOTERID', 'General_2018_11_06'))
+### merge in 2017 election records from 2019 file to replace missings in 2020 file
+hist_2019<-select(hist_2019, c('LALVOTERID', 'General_2017_11_07'))
 vote_poll_demog_addr_all<-vote_poll_demog_addr_all%>%
   # join on L2 voterid
   inner_join(hist_2019, by='LALVOTERID')%>%
   # coalesce to replace missing values
-  mutate(General_2018_11_06 = coalesce(General_2018_11_06.x, General_2018_11_06.y),
+  mutate(General_2017_11_07 = coalesce(General_2017_11_07.x, General_2017_11_07.y),
          Voters_StateVoterID = coalesce(Voters_StateVoterID.x, Voters_StateVoterID.y))%>%
   # remove duplicate variables
-  select(-c(General_2018_11_06.x, General_2018_11_06.y, X, Voters_StateVoterID.x, 
+  select(-c(General_2017_11_07.x, General_2017_11_07.y, X, Voters_StateVoterID.x, 
             Voters_StateVoterID.y))
 
 ### Merge in religious census data on county name
