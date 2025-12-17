2fd1cd6fae069ce0b929a8968c05bfb4eee6f12d
nsflemming <70294403+nsflemming@users.noreply.github.com>
2025-12-16 22:14:35 -0500

    Update DiD_regression_manually.R
    Implement MatchIt based DiD.

--- DiD_regression_manually.R
+++ DiD_regression_manually.R
@@ -13,6 +13,7 @@ library(stringr) #id string manipulation
 library(MatchIt) #matching for DiD
 #library(PSAgraphics)
 library(marginaleffects) #estimates effects with robust standard errors
+library(cobalt)# Check balance
 
 ########## Functions
 ###
@@ -206,7 +207,8 @@ common_covars <-c(
   'pred_race',
   'CommercialData_EstimatedHHIncomeAmount','Residence_Families_HHCount',
   'known_religious','CommercialData_LikelyUnion', 
-  'CommercialData_OccupationGroup',
+  #'CommercialData_OccupationGroup',
+  #''CommercialData_OccupationIndustry',
   'Shape_Length',
   'years_reg'
 )
@@ -248,15 +250,20 @@ model_data$CommercialData_LikelyUnion<-as.factor(model_data$CommercialData_Likel
 #occupational group (Not a variable in 2017 data, so have to remove if using that year)
 model_data$CommercialData_OccupationGroup<-as.factor(model_data$CommercialData_OccupationGroup)
 model_data$CommercialData_OccupationGroup<-relevel(model_data$CommercialData_OccupationGroup, ref='Blue Collar')
+#occupational Industry (substitute for occupation group)
+model_data$CommercialData_OccupationIndustry<-as.factor(model_data$CommercialData_OccupationIndustry)
+model_data$CommercialData_OccupationIndustry<-relevel(model_data$CommercialData_OccupationIndustry, ref='Unknown')
+
 
 ### Create treatment indicators
 two_data<-model_data%>%
   # group by voter
   group_by(LALVOTERID)%>%
-  mutate(ever_changed_poll_loc=(sum(changed_poll_loc)>0),
-         ever_moved_new_poll_loc=(sum(moved_new_poll_loc)>0),
-         ever_no_move_new_poll_loc=(sum(no_move_new_poll_loc)>0),
-         ever_moved_old_poll_loc=(sum(moved_old_poll_loc)>0))%>% 
+  mutate(#ever_changed_poll_loc=(sum(changed_poll_loc)>0),
+         #ever_moved_new_poll_loc=(sum(moved_new_poll_loc)>0),
+         ever_no_move_new_poll_loc=(sum(no_move_new_poll_loc)>0)
+         #ever_moved_old_poll_loc=(sum(moved_old_poll_loc)>0)
+         )%>% 
   #create single variable that indicates if someone voted in a given year
   group_by(year)%>%
  #mutate(voted = ((year==2018 & General_2018_11_06==1)|(year==2019 & General_2019_11_05==1)),
@@ -286,11 +293,12 @@ no_chng_or_no_mv_two_data<-two_data%>%
 # remove missing to prevent different length Y1 and Y0
 ## Test with subsample
 no_chng_or_no_mv_two_data<-no_chng_or_no_mv_two_data%>%
-  select(all_of(c('voted',dvar,'LALVOTERID','VOTERID','year'
+  select(all_of(c('voted',dvar,'LALVOTERID','VOTERID','year','County'
                   ,'Voters_Gender'
                   ,'Voters_Age','Parties_Description','pred_race',
                   'CommercialData_EstimatedHHIncomeAmount','Residence_Families_HHCount',
                   'known_religious','CommercialData_LikelyUnion',
+                  'CommercialData_OccupationIndustry',
                   'years_reg'
   )))%>%
   filter(complete.cases(.))%>%
@@ -303,7 +311,7 @@ no_chng_or_no_mv_two_data<-no_chng_or_no_mv_two_data%>%
 # # Create a mini dataframe for testing by sampling from treated and untreated groups
 mini<-no_chng_or_no_mv_two_data%>%
   group_by('ever_no_move_new_poll_loc')%>%
-  slice_sample(n=500000)%>%
+  slice_sample(n=1000000)%>%
   ungroup()%>%
   mutate(treat=as.integer(ever_no_move_new_poll_loc),
          Voters_Gender=case_when(
@@ -315,8 +323,9 @@ mini<-no_chng_or_no_mv_two_data%>%
          Parties_Description=as.integer(Parties_Description),
          pred_race=as.integer(pred_race),
          known_religious=as.integer(known_religious),
-         CommercialData_LikelyUnion=as.integer(CommercialData_LikelyUnion)
+         CommercialData_LikelyUnion=as.integer(CommercialData_LikelyUnion),
          #CommercialData_OccupationGroup=as.integer(CommercialData_OccupationGroup)
+         CommercialData_OccupationIndustry=as.integer(CommercialData_OccupationIndustry)
   )
 
 # full<-no_chng_or_no_mv_two_data%>%
@@ -339,11 +348,12 @@ mini<-no_chng_or_no_mv_two_data%>%
 ## Filter data for year 2017
 mini_2017 <- mini %>%
   filter(year == 2017) %>%
-  select(all_of(c('LALVOTERID', 'Voters_Gender', 'Voters_Age', 'Parties_Description', 
+  select(all_of(c('LALVOTERID','County','Voters_Gender', 'Voters_Age', 'Parties_Description', 
                   'pred_race','CommercialData_EstimatedHHIncomeAmount', 
                   'Residence_Families_HHCount','known_religious', 
                   'CommercialData_LikelyUnion', 
                   #'CommercialData_OccupationGroup',
+                  'CommercialData_OccupationIndustry',
                   'years_reg')))
 ## Join back to the original dataset
 mini <- mini %>%
@@ -358,6 +368,7 @@ mini <- mini %>%
     known_religious = known_religious_2017,
     CommercialData_LikelyUnion = CommercialData_LikelyUnion_2017,
     #CommercialData_OccupationGroup = CommercialData_OccupationGroup_2017,
+    CommercialData_OccupationIndustry = CommercialData_OccupationIndustry_2017,
     years_reg = years_reg_2017
   )%>%
   select(-ends_with("_2017"))%>%  # Remove extra columns
@@ -366,86 +377,93 @@ mini <- mini %>%
 
 # Logistic Regression based scores
 ## Change in polling location without moving
-ps_formula <- treat ~ Voters_Gender + Voters_Age + Parties_Description+
+ps_formula <- treat ~ County +Voters_Gender + Voters_Age + Parties_Description+
   pred_race+CommercialData_EstimatedHHIncomeAmount+Residence_Families_HHCount+
-  known_religious+CommercialData_LikelyUnion+
+  known_religious+CommercialData_LikelyUnion+CommercialData_OccupationIndustry+
   years_reg
-lr_out <- glm(formula = ps_formula,
-              data = mini,
-              family = binomial(link = 'logit'))
 
-## Add propensity scores to data frame
-mini$lr_ps <- fitted(lr_out)
-
-## Examine distribution of propensity scores
-ggplot(full, aes(x = lr_ps, color = as.logical(ever_no_move_new_poll_loc))) + 
-  geom_density() +
-  guides(col=guide_legend(title='Treated'))+
-  xlab('Propensity Score')
-
-# Stratify into quintiles and convert all variables to numeric
-full <- full %>%
-  mutate(ps_quintile = ntile(lr_ps, 5))
-
-# Get the actual quintile cutpoints
-quintile_cutpoints <- quantile(mini$lr_ps, probs = seq(0, 1, 0.2))
-
-## Check stratification
-table(mini$treat, mini$ps_quintile)
-## Visalize quantiles
-ggplot(mini, aes(x = lr_ps, color = as.logical(treat))) + 
-  geom_density() +
-  guides(col=guide_legend(title='Treated'))+
-  xlab('Propensity Score')+
-  geom_vline(xintercept = quintile_cutpoints)
-
-# Check balance
-## Simple comparison of means within strata
-PSAgraphics::box.psa(continuous = mini$years_reg, 
-                     treatment = mini$treat, 
-                     strata = mini$ps_quintile,
-                     xlab = "Strata", 
-                     balance = FALSE)
-## Simple comparison of proportions within strata
-PSAgraphics::cat.psa(categorical = mini$CommercialData_OccupationGroup, 
-                     treatment = mini$ever_no_move_new_poll_loc, 
-                     strata = mini$ps_quintile, 
-                     xlab = 'Strata',
-                     balance = FALSE)
-
-## Compare covariate effect size to check balance
-covars <- c('treat','Voters_Gender','Voters_Age', 
-            'Parties_Description','pred_race',
-            'CommercialData_EstimatedHHIncomeAmount','Residence_Families_HHCount',
-              'known_religious','CommercialData_LikelyUnion',
-            #'CommercialData_OccupationGroup',
-              'years_reg')
-covars <- data.frame(full[,covars[-1]])
-#All variables must be numeric for the function to work
-PSAgraphics::cv.bal.psa(covariates = covars, 
-                        treatment = full$treat,
-                        propensity = full$lr_ps,
-                        strata = full$ps_quintile)
+#' lr_out <- glm(formula = ps_formula,
+#'               data = mini,
+#'               family = binomial(link = 'logit'))
+#' 
+#' ## Add propensity scores to data frame
+#' mini$lr_ps <- fitted(lr_out)
+#' 
+#' ## Examine distribution of propensity scores
+#' ggplot(mini, aes(x = lr_ps, color = as.logical(ever_no_move_new_poll_loc))) + 
+#'   geom_density() +
+#'   guides(col=guide_legend(title='Treated'))+
+#'   xlab('Propensity Score')
+#' 
+#' # Stratify into quintiles and convert all variables to numeric
+#' mini <- mini %>%
+#'   mutate(ps_quintile = ntile(lr_ps, 5))
+#' 
+#' # Get the actual quintile cutpoints
+#' quintile_cutpoints <- quantile(mini$lr_ps, probs = seq(0, 1, 0.2))
+#' 
+#' ## Check stratification
+#' table(mini$treat, mini$ps_quintile)
+#' ## Visalize quantiles
+#' ggplot(mini, aes(x = lr_ps, color = as.logical(treat))) + 
+#'   geom_density() +
+#'   guides(col=guide_legend(title='Treated'))+
+#'   xlab('Propensity Score')+
+#'   geom_vline(xintercept = quintile_cutpoints)
+#' 
+#' # Check balance
+#' ## Simple comparison of means within strata
+#' PSAgraphics::box.psa(continuous = mini$years_reg, 
+#'                      treatment = mini$treat, 
+#'                      strata = mini$ps_quintile,
+#'                      xlab = "Strata", 
+#'                      balance = FALSE)
+#' ## Simple comparison of proportions within strata
+#' PSAgraphics::cat.psa(categorical = mini$CommercialData_OccupationGroup, 
+#'                      treatment = mini$ever_no_move_new_poll_loc, 
+#'                      strata = mini$ps_quintile, 
+#'                      xlab = 'Strata',
+#'                      balance = FALSE)
+#' 
+#' ## Compare covariate effect size to check balance
+#' covars <- c('treat','Voters_Gender','Voters_Age','County',
+#'             'Parties_Description','pred_race',
+#'             'CommercialData_EstimatedHHIncomeAmount','Residence_Families_HHCount',
+#'               'known_religious','CommercialData_LikelyUnion',
+#'             #'CommercialData_OccupationGroup',
+#'               'years_reg')
+#' covars <- data.frame(mini[,covars[-1]])
+#' #All variables must be numeric for the function to work
+#' PSAgraphics::cv.bal.psa(covariates = covars, 
+#'                         treatment = mini$treat,
+#'                         propensity = mini$lr_ps,
+#'                         strata = mini$ps_quintile)
 
 
 ########## DiD
-# Matching from bryer book
-
 # Matching using MatchIt package
+## Default logistic regression calculated propensity score matching
+### no replacement, one control to one treated
+m.logit<-matchit(ps_formula,data=mini, replace=F)
+summary(m.logit)
+# Check balance
+love.plot(m.logit, drop.distance = TRUE)
 ## Mahalanobis Metric Matching
 ### matches observation to nearest, not exact match
-m.mahal<-matchit(ps_formula,data=mini, replace=FALSE,distance="mahalanobis")
+m.mahal<-matchit(ps_formula,data=mini, replace=F,distance="mahalanobis")
 summary(m.mahal)
 # Check balance
-library(cobalt)
 love.plot(m.mahal, drop.distance = TRUE)
 ## Convert match object into a dataset
 mahal.match<-match.data(m.mahal)
 ## t-test comparing outcome
 t.test(voted ~ treat, data = mahal.match)
 
+
+
+
 #Linear model with covariates (covariates not necessarily needed if balance is good enough)
-fit1 <- glm(voted ~ treat * (Voters_Gender+Voters_Age+Parties_Description+pred_race+
+fit1 <- glm(voted ~ treat * (County+Voters_Gender+Voters_Age+Parties_Description+pred_race+
                                CommercialData_EstimatedHHIncomeAmount+Residence_Families_HHCount+
                                known_religious+CommercialData_LikelyUnion+years_reg),
               data = mahal.match,
@@ -465,16 +483,131 @@ avg_comparisons(fit1,
 ## PSM nearest neighbor
 ##m.nn<-matchit(lalonde.formu, data = lalonde, caliper=0.1, method ="nearest")
 m.nn<-matchit(ps_formula, data = mini, ratio = 1, method ="nearest")
+summary(m.nn)
+# Check balance
+love.plot(m.nn, drop.distance = TRUE)
+# Convert to data frame
 nn.match<-match.data(m.nn)
+## t-test comparing outcome
+t.test(voted ~ treat, data = nn.match)
+#Linear model with covariates (covariates not necessarily needed if balance is good enough)
+fit2 <- glm(voted ~ treat * (County+Voters_Gender+Voters_Age+Parties_Description+pred_race+
+                               CommercialData_EstimatedHHIncomeAmount+Residence_Families_HHCount+
+                               known_religious+CommercialData_LikelyUnion+years_reg),
+            data = nn.match,
+            family = binomial(link = 'logit'),
+            weights = weights)
+#estimate ATT
+avg_comparisons(fit2,
+                variables = "treat",
+                vcov = ~subclass,
+                newdata = subset(treat == 1),
+                # Calculate relative risk
+                comparison = "lnratioavg",
+                transform = "exp")
 
 
+########################### subsets for specific knowledge tests 
+### Parents who changed polling station
+voters_parents_two_data<-two_data%>%
+  # only people who have changed polling location
+  filter(ever_changed_poll_loc==T)%>%
+  # only parents
+  filter(has_child==T)%>%
+  group_by(LALVOTERID)%>%
+  mutate(
+    # Whether new polling location is a school
+    parent_new_poll_school = sum(((location_category=='school')&(year==2019)),
+                                 na.rm=T)>0
+  )%>%
+  ungroup()
 
 
+voters_parents_two_data['parent_new_poll_school'] <- sapply(voters_parents_two_data['parent_new_poll_school'],as.numeric)
+voters_parents_two_data$voted<-as.numeric(voters_parents_two_data$voted)
 
 
+mini_parent<-voters_parents_two_data%>%
+  group_by(parent_new_poll_school)%>%
+  ungroup()%>%
+  mutate(treat=as.integer(parent_new_poll_school),
+         Voters_Gender=case_when(
+           Voters_Gender=='M' ~ 0,
+           Voters_Gender=='F'~1,
+           .default = NA
+         ),
+         Voters_Age=as.integer(Voters_Age),
+         Parties_Description=as.integer(Parties_Description),
+         pred_race=as.integer(pred_race),
+         known_religious=as.integer(known_religious),
+         CommercialData_LikelyUnion=as.integer(CommercialData_LikelyUnion)
+         #CommercialData_OccupationGroup=as.integer(CommercialData_OccupationGroup)
+  )
 
+# fix covariates at 2017 values
+## Filter data for year 2017
+mini_parent_2017 <- mini_parent %>%
+  filter(year == 2017) %>%
+  select(all_of(c('LALVOTERID','County','Voters_Gender', 'Voters_Age', 'Parties_Description', 
+                  'pred_race','CommercialData_EstimatedHHIncomeAmount', 
+                  'Residence_Families_HHCount','known_religious', 
+                  'CommercialData_LikelyUnion', 
+                  #'CommercialData_OccupationGroup',
+                  'years_reg')))
+## Join back to the original dataset
+mini_parent <- mini_parent %>%
+  left_join(mini_parent_2017, by = "LALVOTERID", suffix = c("", "_2017"))%>%
+  mutate(
+    Voters_Gender = Voters_Gender_2017,
+    Voters_Age = Voters_Age_2017,
+    Parties_Description = Parties_Description_2017,
+    pred_race = pred_race_2017,
+    CommercialData_EstimatedHHIncomeAmount = CommercialData_EstimatedHHIncomeAmount_2017,
+    Residence_Families_HHCount = Residence_Families_HHCount_2017,
+    known_religious = known_religious_2017,
+    CommercialData_LikelyUnion = CommercialData_LikelyUnion_2017,
+    #CommercialData_OccupationGroup = CommercialData_OccupationGroup_2017,
+    years_reg = years_reg_2017
+  )%>%
+  select(-ends_with("_2017"))%>%  # Remove extra columns
+  # Remove voters who have missing data
+  filter(complete.cases(.))
 
+## PSM nearest neighbor
+##m.nn<-matchit(lalonde.formu, data = lalonde, caliper=0.1, method ="nearest")
+m.mahal<-matchit(ps_formula,data=mini_parent, replace=T,distance="mahalanobis")
+summary(m.mahal)
 
+m.nn<-matchit(ps_formula, data = mini_parent, ratio = 1, method ="nearest")
+summary(m.nn)
+plot(m.nn, type = "jitter", interactive = FALSE)
+plot(m.nn, type = "density", interactive = FALSE,
+     which.xs = ~ County +Voters_Gender + Voters_Age + Parties_Description+
+       pred_race+CommercialData_EstimatedHHIncomeAmount+Residence_Families_HHCount+
+       known_religious+CommercialData_LikelyUnion+
+       years_reg)
+# Check balance
+love.plot(m.mahal, drop.distance = TRUE)
+love.plot(m.nn, drop.distance = TRUE)
+# Convert to data frame
+mahal.match<-match.data(m.mahal)
+nn.match<-match.data(m.nn)
+## t-test comparing outcome
+t.test(voted ~ treat, data = mahal.match)
+t.test(voted ~ treat, data = nn.match)
+#Linear model with covariates (covariates not necessarily needed if balance is good enough)
+fit_parent <- glm(voted ~ treat * (County+Voters_Gender+Voters_Age+Parties_Description+pred_race+
+                               CommercialData_EstimatedHHIncomeAmount+Residence_Families_HHCount+
+                               known_religious+CommercialData_LikelyUnion+years_reg),
+            data = nn.match,
+            family = binomial(link = 'logit'),
+            weights = weights)
 
-
-
+#estimate ATT
+avg_comparisons(fit_parent,
+                variables = "treat",
+                vcov = ~subclass,
+                newdata = subset(treat == 1),
+                # Calculate relative risk
+                comparison = "lnratioavg",
+                transform = "exp")
