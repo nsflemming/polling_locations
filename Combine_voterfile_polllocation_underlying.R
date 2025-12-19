4df7adef82a1be2b2eb43ffb72ec1797d5ba8d57
nsflemming <70294403+nsflemming@users.noreply.github.com>
2025-12-18 21:39:45 -0500

    Update Combine_voterfile_polllocation_underlying.R
    Add conditional padding of precinct codes for 2017 poll location data.

--- Combine_voterfile_polllocation_underlying.R
+++ Combine_voterfile_polllocation_underlying.R
@@ -19,58 +19,90 @@ pad_code<-function(df, code_var,county_var, counties, code_length){
   return(df)
 }
 
+### Not in function
+'%!in%' <- function(x,y)!('%in%'(x,y))
 
 ################## Main
 ### set directories
 data_dir <- 'C:\\Users\\natha\\Desktop\\Polling Places DiD\\data'
 FVE_dir <- 'C:\\Users\\natha\\Desktop\\Polling Places DiD\\data\\FVE_csvs'
 ### set year
-year='2019'
+year='2017'
 
 ### read in processed voterfile
 VF<-read.csv(paste0(FVE_dir,'\\FVE_',year,'.csv'))
 
 ### read in poll location file
-poll<-read.csv(paste0(data_dir,'\\poll_struct_key_cath_govsource_underlying',substring(year,3,4),'.csv'))
+poll<-read.csv(paste0(data_dir,'\\poll_struct_key_cath_manual_govsource_underlying',substring(year,3,4),'.csv'))
 
 ### merge files on county name and precinct code
 ## modify VF codes to match patterns of polling location codes
+   ########## Need to modify 2017 codes too #######
 ### Clinton, Franklin, Juniata... counties pad to length 2 with 0s on left
-counties2pad<-c('SULLIVAN','ADAMS','ARMSTRONG','BRADFORD','CAMERON','HUNTINGDON',
-                'NORTHUMBERLAND','SOMERSET','SUSQUEHANNA','WYOMING')
+counties2pad<-c('SULLIVAN','ADAMS','ARMSTRONG','BRADFORD','CAMERON','CARBON',
+                'CENTRE',
+                'HUNTINGDON','NORTHUMBERLAND','SOMERSET','SUSQUEHANNA','WYOMING',
+                'LEHIGHTON')
 VF$PrecCode[VF$County%in%counties2pad]<-str_pad(VF$PrecCode[VF$County%in%counties2pad]
                                                  , 2, pad = "0")
+
+if(year=='2017'){poll$PrecinctCode[poll$CountyName%in%counties2pad]<-
+  str_pad(poll$PrecinctCode[poll$CountyName%in%counties2pad], 2, pad = "0")}
+
 ### Clinton, Franklin, Juniata... counties pad to length 3 with 0s on left
-counties3pad<-c('CHESTER','CLINTON','FRANKLIN', 'JUNIATA', 'PERRY','SNYDER','TIOGA','PIKE',
-                'SCHUYLKILL','CLEARFIELD','WAYNE','CRAWFORD')
+counties3pad<-c('CHESTER','CLINTON','FRANKLIN', 'JUNIATA', 'PERRY','SNYDER','TIOGA'
+                ,'PIKE','SCHUYLKILL','CLEARFIELD','WAYNE','CRAWFORD','CAMBRIA')
 VF$PrecCode[VF$County%in%counties3pad]<-str_pad(VF$PrecCode[VF$County%in%counties3pad]
                                                            , 3, pad = "0")
+if(year=='2017'){poll$PrecinctCode[poll$CountyName%in%counties3pad]<-
+  str_pad(poll$PrecinctCode[poll$CountyName%in%counties3pad], 3, pad = "0")}
+
 ### Montour county pad to length 4 with 0s on left
 counties4pad<-c('MONTOUR','FULTON','POTTER','BEDFORD','BEAVER','BUTLER', 'ELK','JEFFERSON',
                 'LAWRENCE','MIFFLIN','COLUMBIA','PHILADELPHIA','MERCER','WASHINGTON',
-                'LYCOMING','WARREN')
+                'LYCOMING','WARREN','CLARION','LANCASTER')
 VF$PrecCode[VF$County%in%counties4pad]<-str_pad(VF$PrecCode[VF$County%in%counties4pad]
                                                 , 4, pad = "0")
+if(year=='2017'){poll$PrecinctCode[poll$CountyName%in%counties4pad]<-
+  str_pad(poll$PrecinctCode[poll$CountyName%in%counties4pad], 4, pad = "0")}
+
 ### Greene, McKean county pad to length 5 with 0s on left
 counties5pad<-c('GREENE','MCKEAN','ERIE','BUCKS','INDIANA','FAYETTE','WESTMORELAND')
 VF$PrecCode[VF$County%in%counties5pad]<-str_pad(VF$PrecCode[VF$County%in%counties5pad]
                                                            , 5, pad = "0")
+if(year=='2017'){poll$PrecinctCode[poll$CountyName%in%counties5pad]<-
+  str_pad(poll$PrecinctCode[poll$CountyName%in%counties5pad], 5, pad = "0")}
 ### Union county pad to length 6 with 0s on left
 counties6pad<-c('UNION','NORTHAMPTON','LUZERNE','DAUPHIN')
 VF$PrecCode[VF$County%in%counties6pad]<-str_pad(VF$PrecCode[VF$County%in%counties6pad]
                                                 , 6, pad = "0")
+if(year=='2017'){poll$PrecinctCode[poll$CountyName%in%counties6pad]<-
+  str_pad(poll$PrecinctCode[poll$CountyName%in%counties6pad], 6, pad = "0")}
+
+### Delware pad to length 8 with 0s on left
+if(year=='2017'){poll$PrecinctCode[poll$CountyName=='DELAWARE']<-
+  str_pad(poll$PrecinctCode[poll$CountyName=='DELAWARE'], 8, pad = "0")}
+
 ### LEBANON, Remove trailing whitespace
 whitespace<-c('LEBANON')
 VF$PrecCode[VF$County%in%whitespace]<-str_trim(VF$PrecCode[VF$County%in%whitespace], side = "right")
 
+
 ### BUTLER, pad both to length 4
 poll$PrecinctCode[poll$CountyName=='BUTLER']<-str_pad(poll$PrecinctCode[poll$CountyName=='BUTLER']
                                                 , 4, pad = "0")
+if(year=='2017'){poll$PrecinctCode[poll$CountyName=='BUTLER']<-
+  str_pad(poll$PrecinctCode[poll$CountyName=='BUTLER'], 4, pad = "0")}
+
 ### Rename McKean to MCKEAN
 poll$CountyName[poll$CountyName=='McKEAN']<-'MCKEAN'
 
+#miss_code_poll<-data.frame(poll$PrecinctCode[poll$PrecinctCode%!in%VF$PrecCode])%>%distinct()
+#miss_code_VF<-data.frame(VF$PrecCode[VF$PrecCode%!in%poll$PrecinctCode])%>%distinct()
+
 # Merge dataframes
 VF_location<-left_join(VF, poll, c('County'='CountyName', 'PrecCode'='PrecinctCode'))
+temp<-VF_location[is.na(VF_location$location_category),]
 
 ## check for missingness
 sum(is.na(VF_location$PrecinctName))
